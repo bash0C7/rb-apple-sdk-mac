4736e0b63079f0d15480bc898f0a4a0ef4624f23
bash0C7 <ksb.4038.nullpointer+github@gmail.com>
2026-05-04 09:37:37 +0900

    test: add failing spec for Pillar 4 ARCBridge

---
+++ test/arc_bridge_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+require "test_helper"
+require "apple_sdk_mac/opaque_ref"
+
+class TestARCBridge < Test::Unit::TestCase
+  def test_opaque_ref_release_called_on_gc
+    counter_handle = AppleSDKMacRuntime.arc_release_counter_init
+    assert_equal 0, AppleSDKMacRuntime.arc_release_counter_value(counter_handle)
+    100.times { _ = AppleSDKMac::OpaqueRef.new(counter_handle) }
+    GC.start
+    GC.start
+    sleep 0.05
+    assert AppleSDKMacRuntime.arc_release_counter_value(counter_handle) > 0
+  end
+end
