3e2900ba3ee30add49bd67bb8b6ffec1704970ce
bash0C7 <ksb.4038.nullpointer+github@gmail.com>
2026-05-06 01:32:59 +0900

    test: failing spec for fields_json importer pipeline pass-through

--- test/test_importer_parameters_json.rb
+++ test/test_importer_parameters_json.rb
@@ -54,4 +54,44 @@ class TestImporterParametersJson < Test::Unit::TestCase
       assert_equal '[{"name":"x","type":"int"}]', sym[:parameters_json]
     end
   end
+
+  class FakeStructHeaderParser
+    def parse_file(_)
+      [{
+        name: "Pt", kind: "struct", abi: "c", parent_name: nil,
+        signature: "struct Pt",
+        fields: [
+          { name: "x", type: "int", kind: "int" },
+          { name: "y", type: "int", kind: "int" }
+        ]
+      }]
+    end
+  end
+
+  def test_pipes_fields_json_into_store_for_struct
+    Dir.mktmpdir do |tmp|
+      headers_dir = File.join(tmp, "Headers")
+      FileUtils.mkdir_p(headers_dir)
+      File.write(File.join(headers_dir, "Pt.h"), "")
+
+      pipeline = AppleSDKKnowledge::Importer::Pipeline.new(store_path: File.join(tmp, "kb.sqlite"))
+      spy = SpyStore.new
+      consolidator = AppleSDKKnowledge::Importer::Consolidator.new
+      pipeline.send(:process_framework,
+                    FakeFramework.new("X", tmp),
+                    spy,
+                    FakeSwiftParser.new,
+                    FakeStructHeaderParser.new,
+                    consolidator,
+                    nil)
+
+      sym = spy.inserted.first
+      assert_not_nil sym, "expected exactly one insert_symbol call"
+      assert_not_nil sym[:fields_json],
+                     "expected fields_json to be passed through for struct symbols"
+      parsed = JSON.parse(sym[:fields_json])
+      assert_equal 2, parsed.length
+      assert_equal "x", parsed[0]["name"]
+    end
+  end
 end
