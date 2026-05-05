199f537eb3550ef8a6324c743f62416f97f1ae4a
bash0C7 <ksb.4038.nullpointer+github@gmail.com>
2026-05-05 09:22:36 +0900

    fix: classify bare 'int' qual_type as int kind, not unsupported

--- lib/rb_apple_sdk_knowledge/importer/kind.rb
+++ lib/rb_apple_sdk_knowledge/importer/kind.rb
@@ -11,7 +11,7 @@ module AppleSDKKnowledge
         return "string" if qual_type =~ /\b(CFStringRef|NSString\s*\*|char\s*\*|const\s+char\s*\*)/
         return "bool"   if qual_type =~ /\b(_Bool|Bool|BOOL|bool)\b/
         return "float"  if qual_type =~ /\b(double|float|CGFloat)\b/
-        if qual_type =~ /\b(?:U?Int(?:8|16|32|64)?|SInt(?:8|16|32|64)?|long|short|unsigned|signed|uint(?:8|16|32|64)_t|int(?:8|16|32|64)_t|OSStatus|kern_return_t)\b/
+        if qual_type =~ /\b(?:int|U?Int(?:8|16|32|64)?|SInt(?:8|16|32|64)?|long|short|unsigned|signed|uint(?:8|16|32|64)_t|int(?:8|16|32|64)_t|OSStatus|kern_return_t)\b/
           return "opaque_ref" if qual_type =~ /\b\w+Ref\b/
           return "int"
         end
