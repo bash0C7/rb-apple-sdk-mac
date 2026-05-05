5f54638d10b59476b640812416924a4dbf7a0c24
bash0C7 <ksb.4038.nullpointer+github@gmail.com>
2026-05-05 09:08:54 +0900

    test: add failing spec for Importer::Kind module surface

---
+++ test/test_kind.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+require "test_helper"
+require "rb_apple_sdk_knowledge/importer/kind"
+
+class TestKind < Test::Unit::TestCase
+  K = AppleSDKKnowledge::Importer::Kind
+
+  def test_classifies_string_for_const_char_pointer
+    assert_equal "string", K.classify_kind("const char *")
+  end
+
+  def test_classifies_string_for_cfstring_ref
+    assert_equal "string", K.classify_kind("CFStringRef")
+  end
+
+  def test_classifies_bool
+    assert_equal "bool", K.classify_kind("_Bool")
+  end
+
+  def test_classifies_float
+    assert_equal "float", K.classify_kind("double")
+  end
+
+  def test_classifies_int_for_osstatus
+    assert_equal "int", K.classify_kind("OSStatus")
+  end
+
+  def test_classifies_opaque_ref_for_ref_typedef
+    assert_equal "opaque_ref", K.classify_kind("MIDIClientRef")
+  end
+
+  def test_classifies_unsupported_for_void_pointer
+    assert_equal "unsupported", K.classify_kind("void *")
+  end
+
+  def test_classifies_unsupported_for_function_pointer_via_desugared
+    assert_equal "unsupported",
+      K.classify_kind("MIDINotifyProc", "void (*)(const MIDINotification *, void *)")
+  end
+
+  def test_out_param_true_for_last_pointer
+    assert_equal true, K.out_param?("MIDIClientRef *", "outClient", true)
+  end
+
+  def test_out_param_true_for_out_prefix_name
+    assert_equal true, K.out_param?("Int32 *", "outNode", false)
+  end
+
+  def test_out_param_false_for_non_pointer
+    assert_equal false, K.out_param?("CFStringRef", "name", false)
+  end
+
+  def test_nullability_nonnull
+    assert_equal "nonnull", K.nullability_of("CFStringRef _Nonnull")
+  end
+
+  def test_nullability_nullable
+    assert_equal "nullable", K.nullability_of("MIDIClientRef _Nullable")
+  end
+
+  def test_nullability_unspecified
+    assert_equal "unspecified", K.nullability_of("CFStringRef")
+  end
+end
